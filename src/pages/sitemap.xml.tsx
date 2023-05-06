b60c5afa6bf04d0d22e24488f90548149189898f
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-05-06 21:56:54 +0530

    updated sitemap

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -12,22 +12,22 @@ function generateSiteMap() {
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
               http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">  
       <url>
-          <loc>https://arupbasak.co.in/</loc>
+          <loc>https://arupbasak.com/</loc>
           <lastmod>2023-04-01T14:50:39+00:00</lastmod>
           <priority>1.00</priority>
       </url>
       <url>
-          <loc>https://arupbasak.co.in/projects</loc>
+          <loc>https://arupbasak.com/projects</loc>
           <lastmod>2023-04-01T14:50:39+00:00</lastmod>
           <priority>0.80</priority>
       </url>
       <url>
-          <loc>https://arupbasak.co.in/blogs</loc>
+          <loc>https://arupbasak.com/blogs</loc>
           <lastmod>2023-04-01T14:50:39+00:00</lastmod>
-          <priority>0.80</priority>
+          <priority>0.30</priority>
       </url>
       <url>
-          <loc>https://arupbasak.co.in/contact</loc>
+          <loc>https://arupbasak.com/contact</loc>
           <lastmod>2023-04-01T14:50:39+00:00</lastmod>
           <priority>0.80</priority>
       </url>
