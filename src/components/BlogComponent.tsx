a4a7f6ddaf27f6530dbab02e2c15f70ded3850b9
Arup Basak <95271566+arup-basak@users.noreply.github.com>
2023-06-25 11:56:57 +0530

    added Framer Motion in blogcomponent

--- src/components/BlogComponent.tsx
+++ src/components/BlogComponent.tsx
@@ -1,29 +1,45 @@
 import Link from 'next/link'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 interface Blog {
-    _id: any,
-    heading: string,
-    pid: string,
-    data: string,
-    time: string,
-    writer: string
-  }
+  _id: any,
+  heading: string,
+  pid: string,
+  data: string,
+  time: string,
+  writer: string
+}
 
 const BlogComponent = (props: {
-    blog: Blog
+  blog: Blog
 }) => {
   return (
-    <div className='rounded-xl bg-gray-300 p-2 m-2 cursor-pointer'>
-        <Link href={`blog/${props.blog.pid}`}>
-            <div>
-                {props.blog.heading}
-            </div>
-            <div className='text-lg'>
-                {props.blog.data}
-            </div>
-        </Link>
-    </div>
+    <motion.div 
+      initial={{
+        scale: 0,
+        opacity: 0
+      }}
+      animate={{
+        opacity: 1,
+        scale: 1
+      }}
+      whileHover={{
+        scale: 1.02
+      }}
+      whileTap={{
+        scale: .98
+      }}
+      className='rounded-xl bg-gray-300 p-2 m-2 cursor-pointer'>
+      <Link href={`blog/${props.blog.pid}`}>
+        <div>
+          {props.blog.heading}
+        </div>
+        <div className='text-lg'>
+          {props.blog.data}
+        </div>
+      </Link>
+    </motion.div>
   )
 }
 
